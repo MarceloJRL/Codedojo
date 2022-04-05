4fa4c1873caac18cdc9191af0e80064471b57944
Marcelo Ramirez <80780366+MarceloJRL@users.noreply.github.com>
2022-04-05 14:41:02 -0500

    Add files via upload

---
+++ ninja.js
@@ -0,0 +1,21 @@
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+
+
+
+class ninja{
+    {constructor(nombre, salud, velocidad, fuerza)
+
+    this.nombre=nombre
+    this.salud=3
+    this.velocidad=velocidad
+    this.fuerza=fuerza
+    }
+
+
+    ninja.sayName(){console.log=nombre};
+    ninja.showStats(){console.log=nombre,fuerza,velocidad,salud};
+    ninja.drinkSake(){this.salud +=10;};
+    }
